5d101b75699cd7f7b1d1bbf31fa0555e2b5fd16f
Davi Pessoa de Melo <60262162+pessoadavi@users.noreply.github.com>
2022-09-19 23:50:46 -0300

    Correção de implementação.

--- src/main/java/com/testes/apitestes/model/dto/UserDTO.java
+++ src/main/java/com/testes/apitestes/model/dto/UserDTO.java
@@ -7,8 +7,10 @@ import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class UserDTO implements Serializable{
 
